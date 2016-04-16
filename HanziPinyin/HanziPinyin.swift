3cd19e940020b4ffc86b33f64b9fc2f5e1454857
Hong Xin <hongxinhope@gmail.com>
2016-04-16 17:56:45 +0800

    Initial Commit

---
+++ HanziPinyin/HanziPinyin.swift
@@ -0,0 +1,9 @@
+//
+//  HanziPinyin.swift
+//  HanziPinyin
+//
+//  Created by Xin Hong on 16/4/16.
+//  Copyright © 2016年 Teambition. All rights reserved.
+//
+
+import Foundation
---
+++ HanziPinyinExample/HanziPinyinExampleViewController.swift
@@ -0,0 +1,16 @@
+//
+//  HanziPinyinExampleViewController.swift
+//  HanziPinyinExample
+//
+//  Created by Xin Hong on 16/4/16.
+//  Copyright © 2016年 Teambition. All rights reserved.
+//
+
+import UIKit
+
+class HanziPinyinExampleViewController: UIViewController {
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
+
